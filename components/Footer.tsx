5a6b2fca0fa44d8d91b6b13a46d60c81f852487f
hamhyeonggwang <h2g0614@gmail.com>
2025-06-26 06:44:51 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
+import cs from 'classnames'
 import { IoMoonSharp } from '@react-icons/all-files/io5/IoMoonSharp'
 import { IoSunnyOutline } from '@react-icons/all-files/io5/IoSunnyOutline'
-import cs from 'classnames'
 
 import { useDarkMode } from '@/lib/use-dark-mode'
 import styles from './styles.module.css'
@@ -29,7 +29,7 @@ function ToggleThemeButton() {
   )
 }
 
-export const Footer: React.FC = () => {
+export function Footer(): JSX.Element {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>
